87be17e17a66e38dc9709873fb17b4d087678f8f
ClayData <claytonortiz96@gmail.com>
2020-01-26 20:50:08 -0600

    jumbotron weather listing

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
+
+m = moment().format('L')
+
 console.log("minneapolis");
 $(".btn").on("click", function(){
     console.log("minneapolis");
     var givenCity = $("#city-input").val();
     console.log(givenCity)
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + givenCity + ",us&mode=json&appid=e9d14d7bd92938f37390ec154c374984"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + givenCity + ",us&units=imperial&mode=json&appid=e9d14d7bd92938f37390ec154c374984"
 
 
     $.ajax({
@@ -14,9 +17,18 @@ $(".btn").on("click", function(){
       }).then(function(response){
 
         console.log(response)
-
+        $("#jumbo-temp").text("Temp: " + JSON.stringify(response.main.temp))
+        $("#jumbo-humid").text("Humidity: " + JSON.stringify(response.main.humidity))
+        $("#jumbo-wind").text("Wind Speed: " + JSON.stringify(response.wind.speed))
+        
       })
+
+      $("#jumbo-city").text(givenCity + " " + m);
 
    
 });
+
+
+
+
 });
